b35b4579e73ffc9e6adc56b1449e0c00332ed8ae
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-05-04 08:02:45 +0530

    Time: 2 ms (61.75%), Space: 26 MB (98.75%) - LeetHub

--- 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -1,12 +1,20 @@
-class Solution {
-public:
-    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        int cnt[100]{};
-        int ans = 0;
-        for (auto& e : dominoes) {
-            int x = e[0] < e[1] ? e[0] * 10 + e[1] : e[1] * 10 + e[0];
-            ans += cnt[x]++;
+struct Solution
+{
+    static constexpr int numEquivDominoPairs(
+            std::span<const std::vector<int>> dominoes) noexcept
+    {
+        std::array<int, 100> counter = {};
+        for (auto& domino: dominoes)
+        {
+            const int normalized = (domino[0] < domino[1])
+                    ? domino[0] * 10 + domino[1]
+                    : domino[1] * 10 + domino[0];
+            ++counter[normalized];
         }
-        return ans;
+        return std::accumulate(
+                begin(counter), end(counter), 0,
+                [](auto acc, auto count) {
+                    return acc + count * (count - 1) / 2;
+                });
     }
 };
