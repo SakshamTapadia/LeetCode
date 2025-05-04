46cdfb033904ab281135bc9459f8533459cacc66
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-05-05 01:09:25 +0530

    Time: 3 ms (96.9%), Space: 12.5 MB (53.9%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.py
+++ 0326-power-of-three/0326-power-of-three.py
@@ -4,8 +4,4 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        while n>2:
-            if n%3:
-                return False
-            n//=3
-        return n==1
+        return n>0 and 1162261467%n==0
