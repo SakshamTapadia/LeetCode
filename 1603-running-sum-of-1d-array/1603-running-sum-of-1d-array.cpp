f38a0b3041fa46586cf97584a24a3b23581651ad
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-05-05 00:42:54 +0530

    Time: 0 ms (100%), Space: 12.6 MB (49.34%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> runningSum(vector<int>& nums) {
         vector<int> prefix;
         int sumFromLeft = 0;
-        for (int num : nums) {  //for-each loop; num acts as a pointer pointing to each element in nums
+        for (int num : nums) { 
             sumFromLeft += num;
             prefix.push_back(sumFromLeft);
         }
