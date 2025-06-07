574da337cd14b551461a97e012d8001ac3886fb4
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-07 11:42:42 +0530

    Time: 1 ms (98.86%), Space: 11.5 MB (61.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,26 +1,38 @@
-class Solution {
+class Solution{
 public:
-    vector<int> longestPalindromeBetween(const string& s, int i, int j) {
-        while (i >= 0 && j < s.length()) {
-            if (s[i] != s[j]) {
-                break;
-            }
-            i--;
-            j++;
+    vector<int> manachers_algo(string s){
+        string newS = "#";
+        for(char c: s){
+            newS += string(1, c) + "#";
         }
-        return vector<int> {i+1, j};
+        int n = newS.size();
+        vector<int> arrLPS(n, 0);
+        int l = 0, r = 0;
+        for(int i = 0; i < n; i++){
+            if(r - i > 0) arrLPS[i] = min(r-i, arrLPS[l + r - i]);
+
+            while(i-arrLPS[i]-1 >= 0 && i+arrLPS[i]+1 < n && newS[i-arrLPS[i]-1]==newS[i+arrLPS[i]+1]){
+                arrLPS[i]++;
+            }
+            if(i+arrLPS[i] > r){
+                l = i - arrLPS[i];
+                r = i + arrLPS[i];
+            }
+        }
+        return arrLPS;
     }
-    
-    string longestPalindrome(string s) {
-        string result = "";
-        for (int i = 0; i < s.length(); i++) {
-            vector<int> odd = longestPalindromeBetween(s, i-1, i+1);
-            vector<int> even = longestPalindromeBetween(s, i-1, i);
-            vector<int> longest = odd[1] - odd[0] > even[1] - even[0] ? odd : even;
-            if (result.length() < longest[1] - longest[0]) {
-                result = s.substr(longest[0], longest[1] - longest[0]);
+
+    string longestPalindrome(string s){
+        vector<int> arrLPS = manachers_algo(s);
+        int maxLPS = arrLPS[0];
+        int center = 0;
+        for(int i = 1; i < arrLPS.size(); i++){
+            if(arrLPS[i] > maxLPS){
+                maxLPS = arrLPS[i];
+                center = i;
             }
         }
-        return result;
+        int beginIndex = (center-maxLPS)/2;
+        return s.substr(beginIndex, maxLPS);
     }
 };
