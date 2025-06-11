8664d7cf6acdfcabdd9ab913427c260bf53be61c
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-11 11:04:52 +0530

    Time: 31 ms (96.15%), Space: 11.7 MB (93.59%) - LeetHub

--- 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
+++ 3761-maximum-difference-between-even-and-odd-frequency-ii/3761-maximum-difference-between-even-and-odd-frequency-ii.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int maxDifference(string s, int k) {
-        const int n = s.size();
-        const int inf = INT_MAX / 2;
-        int ans = -inf;
+        auto getStatus = [](int cnt_a, int cnt_b) -> int {
+            return ((cnt_a & 1) << 1) | (cnt_b & 1);
+        };
 
-        for (int a = 0; a < 5; ++a) {
-            for (int b = 0; b < 5; ++b) {
+        int n = s.size();
+        int ans = INT_MIN;
+        for (char a = '0'; a <= '4'; ++a) {
+            for (char b = '0'; b <= '4'; ++b) {
                 if (a == b) {
                     continue;
                 }
-
-                int curA = 0, curB = 0;
-                int preA = 0, preB = 0;
-                int t[2][2] = {{inf, inf}, {inf, inf}};
-                int l = -1;
-
-                for (int r = 0; r < n; ++r) {
-                    curA += (s[r] == '0' + a);
-                    curB += (s[r] == '0' + b);
-                    while (r - l >= k && curB - preB >= 2) {
-                        t[preA & 1][preB & 1] = min(t[preA & 1][preB & 1], preA - preB);
-                        ++l;
-                        preA += (s[l] == '0' + a);
-                        preB += (s[l] == '0' + b);
+                int best[4] = {INT_MAX, INT_MAX, INT_MAX, INT_MAX};
+                int cnt_a = 0, cnt_b = 0;
+                int prev_a = 0, prev_b = 0;
+                int left = -1;
+                for (int right = 0; right < n; ++right) {
+                    cnt_a += (s[right] == a);
+                    cnt_b += (s[right] == b);
+                    while (right - left >= k && cnt_b - prev_b >= 2) {
+                        int left_status = getStatus(prev_a, prev_b);
+                        best[left_status] =
+                            min(best[left_status], prev_a - prev_b);
+                        ++left;
+                        prev_a += (s[left] == a);
+                        prev_b += (s[left] == b);
                     }
-                    ans = max(ans, curA - curB - t[(curA & 1) ^ 1][curB & 1]);
+                    int right_status = getStatus(cnt_a, cnt_b);
+                    if (best[right_status ^ 0b10] != INT_MAX) {
+                        ans =
+                            max(ans, cnt_a - cnt_b - best[right_status ^ 0b10]);
+                    }
                 }
             }
         }
-
         return ans;
     }
 };
