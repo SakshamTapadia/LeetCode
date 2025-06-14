e3c46569290e61d5f4d33e91191dcc027b0b1c32
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-14 18:42:19 +0530

    Time: 0 ms (100%), Space: 9.3 MB (6.04%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.cpp
@@ -1,26 +1,45 @@
 class Solution {
+    void f(int num, vector<int>& ds) {
+        while (num > 0) {
+            ds.push_back(num % 10);
+            num /= 10;
+        }
+    }
+
 public:
     int minMaxDifference(int num) {
-        string s = to_string(num);
-        string t = s;
-        char first = s[0];
-        for (char& c : s) {
-            if (c == first) {
-                c = '0';
+        int c = -1;
+        vector<int> ds;
+        f(num, ds);
+        reverse(ds.begin(), ds.end());
+
+        int a = ds[0]; 
+        string as = "", bs = "";
+
+        
+        for (int i : ds) {
+            if (i != 9) {
+                c = i;
+                break;
             }
         }
-        int mi = stoi(s);
-        for (int i = 0; i < t.size(); ++i) {
-            if (t[i] != '9') {
-                char second = t[i];
-                for (int j = i; j < t.size(); ++j) {
-                    if (t[j] == second) {
-                        t[j] = '9';
-                    }
-                }
-                return stoi(t) - mi;
-            }
+
+        
+        for (int i = 0; i < ds.size(); i++) {
+            if (ds[i] == c)
+                as += '9';
+            else
+                as += to_string(ds[i]);
         }
-        return num - mi;
+
+      
+        for (int i = 0; i < ds.size(); i++) {
+            if (a != 0 && ds[i] == a)
+                bs += '0';
+            else
+                bs += to_string(ds[i]);
+        }
+
+        return stoi(as) - stoi(bs);
     }
 };
