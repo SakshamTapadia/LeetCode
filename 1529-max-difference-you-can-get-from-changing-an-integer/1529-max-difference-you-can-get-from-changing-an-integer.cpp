7e1c7fc80c0e39acb78c20fe53fb513e908aa4e9
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-15 20:08:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (69.19%) - LeetHub

--- 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
+++ 1529-max-difference-you-can-get-from-changing-an-integer/1529-max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,31 +1,59 @@
 class Solution {
 public:
     int maxDiff(int num) {
-        auto replace = [](string& s, char a, char b) {
-            for (auto& c : s) {
-                if (c == a) {
-                    c = b;
-                }
+
+        string t = to_string(num);
+        char c;
+
+        bool f = false;
+
+        for (int i = 0; i < t.size(); i++) {
+
+            if (t[i] == '9')
+                continue;
+
+            c = t[i];
+            break;
+        }
+        string p = to_string(num);
+
+        for (int i = 0; i < t.size(); i++) {
+            if (t[i] == c)
+                t[i] = '9';
+        }
+
+        char c1 = p[0];
+        for (int i = 0; i < t.size(); i++) {
+            if (c1 == p[i]) {
+                if (p[i] == '1')
+                    continue;
+                p[i] = '1';
+                f = true;
             }
-        };
-        string a = to_string(num);
-        string b = a;
-        for (int i = 0; i < a.size(); ++i) {
-            if (a[i] != '9') {
-                replace(a, a[i], '9');
+        }
+        if (f) {
+            int p1 = stoi(p);
+            int p2 = stoi(t);
+
+            cout << p1;
+            return p2 - p1;
+        }
+
+        char c3;
+
+        for (int i = 0; i < p.size(); i++) {
+            if (p[i] != '0' && i != 0 && p[i]!=p[0]) {
+                c3 = p[i];
                 break;
             }
         }
-        if (b[0] != '1') {
-            replace(b, b[0], '1');
-        } else {
-            for (int i = 1; i < b.size(); ++i) {
-                if (b[i] != '0' && b[i] != '1') {
-                    replace(b, b[i], '0');
-                    break;
-                }
-            }
+        for (int i = 0; i < p.size(); i++) {
+            if (c3 == p[i] && i!=0)
+                p[i] = '0';
         }
-        return stoi(a) - stoi(b);
+
+        cout << p;
+
+        return stoi(t) - stoi(p);
     }
 };
