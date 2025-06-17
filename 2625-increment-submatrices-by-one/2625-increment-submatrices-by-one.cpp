9ee8388bb2c48336cae86350845e8bc7420940f8
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-17 23:42:17 +0530

    Time: 38 ms (55.72%), Space: 93.2 MB (22.88%) - LeetHub

--- 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
+++ 2625-increment-submatrices-by-one/2625-increment-submatrices-by-one.cpp
@@ -1,27 +1,28 @@
 class Solution {
-public:
-    vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
-        vector<int>row1,row2;
-        vector<int>fir(n+1,0),sec(n+1,0);
-        vector<vector<int>>ans(n,vector<int>(n+1,0));
-        
-        for(auto x : queries){
-            int x1 = x[0], x2 = x[2];
-            int y1 = x[1], y2 = x[3];
-            
-            for(int i = x1; i<=x2; i++){
-                ans[i][y1] +=1;
-                ans[i][y2+1] -=1;
-            }
-        }
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<n; j++){
-                if(j>0) ans[i][j] = ans[i][j]+ans[i][j-1];
-            }
-        }
-        for(int i = 0; i<n; i++) ans[i].pop_back();
+ public:
+  vector<vector<int>> rangeAddQueries(int n, vector<vector<int>>& queries) {
+    vector<vector<int>> ans(n, vector<int>(n));
+    vector<vector<int>> prefix(n, vector<int>(n + 1));
 
-        return ans;
+    for (const vector<int>& query : queries) {
+      const int row1 = query[0];
+      const int col1 = query[1];
+      const int row2 = query[2];
+      const int col2 = query[3];
+      for (int i = row1; i <= row2; ++i) {
+        ++prefix[i][col1];
+        --prefix[i][col2 + 1];
+      }
     }
+
+    for (int i = 0; i < n; ++i) {
+      int sum = 0;
+      for (int j = 0; j < n; ++j) {
+        sum += prefix[i][j];
+        ans[i][j] = sum;
+      }
+    }
+
+    return ans;
+  }
 };
