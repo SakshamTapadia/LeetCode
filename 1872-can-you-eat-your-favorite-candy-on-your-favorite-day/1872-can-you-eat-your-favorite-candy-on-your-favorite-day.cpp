e3826df5f2a0cb18789c7ba243791f085ce0b814
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-17 23:44:47 +0530

    Time: 4 ms (73.96%), Space: 125.8 MB (26.04%) - LeetHub

--- 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.cpp
+++ 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.cpp
@@ -1,15 +1,32 @@
 class Solution {
 public:
     vector<bool> canEat(vector<int>& candiesCount, vector<vector<int>>& queries) {
-        int n = candiesCount.size();
-        vector<long long> s(n + 1);
-        for (int i = 0; i < n; ++i) s[i + 1] = s[i] + candiesCount[i];
-        vector<bool> ans;
-        for (auto& q : queries) {
-            int t = q[0], day = q[1], mx = q[2];
-            long long least = day, most = 1ll * (day + 1) * mx;
-            ans.emplace_back(least < s[t + 1] && most > s[t]);
+        int m = candiesCount.size();
+        int n = queries.size();
+
+        vector<long long> prefix(m + 1, 0); 
+        for (int i = 0; i < m; i++) {
+            prefix[i + 1] = prefix[i] + candiesCount[i];
         }
-        return ans;
+
+        vector<bool> result;
+        for (const auto& q : queries) {
+            int type = q[0];
+            int day = q[1];
+            int cap = q[2];
+
+            long long minCandies = day + 1;                     
+            long long maxCandies = (long long)(day + 1) * cap;  
+
+            long long earliestCandy = prefix[type] + 1; 
+            long long latestCandy = prefix[type + 1];   
+            if (maxCandies >= earliestCandy && minCandies <= latestCandy) {
+                result.push_back(true);
+            } else {
+                result.push_back(false);
+            }
+        }
+
+        return result;
     }
 };
