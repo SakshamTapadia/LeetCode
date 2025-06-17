b162d6cf23ae895ccd46d3421cc72048acb94b42
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-17 23:44:21 +0530

    Time: 4 ms (73.96%), Space: 125.7 MB (45.83%) - LeetHub

---
+++ 1872-can-you-eat-your-favorite-candy-on-your-favorite-day/1872-can-you-eat-your-favorite-candy-on-your-favorite-day.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> canEat(vector<int>& candiesCount, vector<vector<int>>& queries) {
+        int n = candiesCount.size();
+        vector<long long> s(n + 1);
+        for (int i = 0; i < n; ++i) s[i + 1] = s[i] + candiesCount[i];
+        vector<bool> ans;
+        for (auto& q : queries) {
+            int t = q[0], day = q[1], mx = q[2];
+            long long least = day, most = 1ll * (day + 1) * mx;
+            ans.emplace_back(least < s[t + 1] && most > s[t]);
+        }
+        return ans;
+    }
+};
