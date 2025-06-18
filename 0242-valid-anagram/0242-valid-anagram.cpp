38163f3f717b71c4e72b4225569578ca9351c275
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-18 10:55:44 +0530

    Time: 4 ms (39.85%), Space: 9.7 MB (48.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s==t) return true;
-        else return false;
+        if(s.length()!=t.length()){
+            return false;
+        }
+        vector<int> freq(26,0);
+        for(int i=0;i<s.length();i++){
+            freq[s[i]-'a']++;
+            freq[t[i]-'a']--;
+        }
+        for(auto f:freq){
+            if(f) return false;
+        }
+        return true;
     }
 };
