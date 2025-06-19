12342e64a94aa80e611d5963fe44f7f501f74c3a
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-19 11:45:43 +0530

    Time: 8 ms (60.96%), Space: 22.8 MB (73.86%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool possible(vector<int>& piles, int speed, int h) {
+        int time = 0;
+        for (int pile : piles) {
+            time += (pile + speed - 1) / speed;
+            if (time > h)
+                return false;
+        }
+        return true;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int low = 1;
+        int high = *max_element(piles.begin(), piles.end());
+        int ans = high;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (possible(piles, mid, h)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
