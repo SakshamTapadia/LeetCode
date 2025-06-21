cc20ea5d26dbbdf93df1218db188884432dc1dcd
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-21 11:34:09 +0530

    Time: 35 ms (98.63%), Space: 28.9 MB (90.55%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] == nums[i-1]) {
-                continue;
-            }
-            
-            int j = i + 1;
-            int k = nums.size() - 1;
-
-            while (j < k) {
-                int total = nums[i] + nums[j] + nums[k];
-
-                if (total > 0) {
-                    k--;
-                } else if (total < 0) {
-                    j++;
-                } else {
-                    res.push_back({nums[i], nums[j], nums[k]});
-                    j++;
-
-                    while (nums[j] == nums[j-1] && j < k) {
-                        j++;
+        vector<vector<int>> ans; 
+        sort(nums.begin() , nums.end()); 
+        int i=0; 
+        int prev = INT_MIN; 
+        while(i<=nums.size()-3){
+            if(nums[i] == prev) i++; 
+            else{
+                int target = -1*nums[i]; 
+                int j=i+1 , z=nums.size()-1;
+                while(j<z){
+                    int sum = nums[j] + nums[z];
+                    if(sum>target){
+                        z--; 
+                    }
+                    else if(sum<target){
+                        j++; 
+                    }
+                    else{
+                        ans.push_back({nums[i] , nums[j] , nums[z]});
+                        int oj = nums[j]; 
+                        int oz = nums[z];
+                        while(j<z){
+                            if(nums[j] == oj && nums[z] == oz){
+                                j++; 
+                                z--; 
+                            }
+                            else break;
+                        }
                     }
                 }
+                prev = nums[i]; 
+                i++; 
             }
         }
-        return res;        
+        return ans; 
     }
 };
