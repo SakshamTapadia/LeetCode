8e98035d7e9da0312869f21ede91520e1c09e030
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-23 15:00:32 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.42%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
 
 ListNode* reverse(ListNode* head, ListNode* tail) {
-    ListNode* prev = tail; //the prev is the node which is not included in the reversal
+    ListNode* prev = tail;
     while (head != tail) {
         ListNode* next = head->next;
         head->next = prev;
