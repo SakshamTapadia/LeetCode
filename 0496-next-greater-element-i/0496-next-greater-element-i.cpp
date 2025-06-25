a23c94fdd53817f93d76d95ecc7d43ecec7fd462
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-25 09:49:20 +0530

    Time: 0 ms (100%), Space: 12.3 MB (84.47%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        stack<int> stk;
-        unordered_map<int, int> d;
-        ranges::reverse(nums2);
-        for (int x : nums2) {
-            while (!stk.empty() && stk.top() < x) {
-                stk.pop();
+        int i = 0;
+        int n = nums1.size();
+        int m = nums2.size();
+
+        vector<int> result;
+
+        while(i < n){
+            int j = 0;
+
+            // Find nums1[i] in nums2
+            while(j < m && nums1[i] != nums2[j]) j++;
+
+            j++; // Move to the right of nums2[j]
+            while(j < m && nums2[j] <= nums1[i]) j++;
+
+            if(j < m){
+                result.push_back(nums2[j]); // Found greater element
+            } else {
+                result.push_back(-1); // No greater element
             }
-            if (!stk.empty()) {
-                d[x] = stk.top();
-            }
-            stk.push(x);
+
+            i++;
         }
-        vector<int> ans;
-        for (int x : nums1) {
-            ans.push_back(d.contains(x) ? d[x] : -1);
-        }
-        return ans;
+
+        return result;
     }
 };
