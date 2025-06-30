9a2aca45a7e0ae834f18f4fd372e36dca2b99ce6
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-06-30 09:26:04 +0530

    Time: 0 ms (100%), Space: 19.1 MB (45.6%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,6 +15,9 @@ public:
         if (!root) {
             return 0;
         }
-        return 1 + max(maxDepth(root->left), maxDepth(root->right));        
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+
+        return 1 + max(lh,rh);        
     }
 };
