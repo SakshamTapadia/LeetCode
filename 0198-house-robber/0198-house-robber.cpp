089851ca953a148090b50a62105e0cb8d1bb0904
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-07-01 11:15:19 +0530

    Time: 0 ms (100%), Space: 10.8 MB (22.33%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
+    int recursion(vector<int>&nums, int i, vector<int>&dp){
+
+        if(i >= nums.size()) return 0;
+        if(dp[i] != -1) return dp[i];
+
+        int skip = 0, take = 0;
+
+        skip = 0 + recursion(nums, i+1, dp);
+        take = nums[i] + recursion(nums, i + 2, dp);
+
+        return dp[i] = max(skip, take);
+    }
+    
     int rob(vector<int>& nums) {
+        
         int n = nums.size();
-
-        if (n == 1) {
-            return nums[0];
-        }
-
-        vector<int> dp(n, 0);
-
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 1], nums[i] + dp[i - 2]);
-        }
-
-        return dp[n - 1];        
+        vector<int>dp(n + 1, -1);
+        return recursion(nums, 0, dp);
+        
     }
 };
