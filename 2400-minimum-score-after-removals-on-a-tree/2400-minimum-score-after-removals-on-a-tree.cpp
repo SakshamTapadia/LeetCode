e3d623c8df1e0fbd615a877987867f88d884d25b
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-07-24 12:20:58 +0530

    Time: 31 ms (96.83%), Space: 19.3 MB (60.32%) - LeetHub

--- 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
+++ 2400-minimum-score-after-removals-on-a-tree/2400-minimum-score-after-removals-on-a-tree.cpp
@@ -1,63 +1,45 @@
 class Solution {
 public:
-    vector<int> subtree_xor;
-    vector<unordered_set<int>> descendants;
-    vector<vector<int>> graph;
-
-    void dfs(int node, int parent, const vector<int>& nums) {
-        subtree_xor[node] = nums[node];
-        descendants[node].insert(node);
-
-        for (int neighbor : graph[node]) {
-            if (neighbor != parent) {
-                dfs(neighbor, node, nums);
-                subtree_xor[node] ^= subtree_xor[neighbor];
-                descendants[node].insert(descendants[neighbor].begin(), descendants[neighbor].end());
-            }
-        }
+    int calc(int part1, int part2, int part3) {
+        return max(part1, max(part2, part3)) - min(part1, min(part2, part3));
     }
-
     int minimumScore(vector<int>& nums, vector<vector<int>>& edges) {
-        int n = nums.size();
-        graph.assign(n, vector<int>());
-        subtree_xor.assign(n, 0);
-        descendants.assign(n, unordered_set<int>());
-
-        for (const auto& edge : edges) {
-            graph[edge[0]].push_back(edge[1]);
-            graph[edge[1]].push_back(edge[0]);
+        int n = nums.size(), cnt = 0;
+        vector<int> sum(n), in(n), out(n);
+        vector<vector<int>> adj(n);
+        for (auto& e : edges) {
+            adj[e[0]].push_back(e[1]);
+            adj[e[1]].push_back(e[0]);
         }
-
-        dfs(0, -1, nums);
-
-        int total_xor = subtree_xor[0];
-        int min_score = INT_MAX;
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                int xor_i = subtree_xor[i];
-                int xor_j = subtree_xor[j];
-                int val1, val2, val3;
-
-                if (descendants[i].count(j)) { // j is in i's subtree
-                    val1 = xor_j;
-                    val2 = xor_i ^ xor_j;
-                    val3 = total_xor ^ xor_i;
-                } else if (descendants[j].count(i)) { // i is in j's subtree
-                    val1 = xor_i;
-                    val2 = xor_j ^ xor_i;
-                    val3 = total_xor ^ xor_j;
-                } else { // Independent subtrees
-                    val1 = xor_i;
-                    val2 = xor_j;
-                    val3 = total_xor ^ xor_i ^ xor_j;
+        function<void(int, int)> dfs = [&](int x, int fa) {
+            in[x] = cnt++;
+            sum[x] = nums[x];
+            for (auto& y : adj[x]) {
+                if (y == fa) {
+                    continue;
                 }
-                
-                int score = max({val1, val2, val3}) - min({val1, val2, val3});
-                min_score = min(min_score, score);
+                dfs(y, x);
+                sum[x] ^= sum[y];
+            }
+            out[x] = cnt;
+        };
+
+        dfs(0, -1);
+        int res = INT_MAX;
+        for (int u = 1; u < n; u++) {
+            for (int v = u + 1; v < n; v++) {
+                if (in[v] > in[u] && in[v] < out[u]) {
+                    res = min(res,
+                              calc(sum[0] ^ sum[u], sum[u] ^ sum[v], sum[v]));
+                } else if (in[u] > in[v] && in[u] < out[v]) {
+                    res = min(res,
+                              calc(sum[0] ^ sum[v], sum[v] ^ sum[u], sum[u]));
+                } else {
+                    res = min(res,
+                              calc(sum[0] ^ sum[u] ^ sum[v], sum[u], sum[v]));
+                }
             }
         }
-
-        return min_score;
+        return res;
     }
 };
