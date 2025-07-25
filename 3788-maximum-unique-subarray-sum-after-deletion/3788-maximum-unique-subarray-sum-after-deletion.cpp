d0b97f00190acf7d8bb07e4cfb92851276a20a19
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-07-25 13:32:58 +0530

    Time: 2 ms (30.41%), Space: 29.6 MB (37.84%) - LeetHub

--- 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
+++ 3788-maximum-unique-subarray-sum-after-deletion/3788-maximum-unique-subarray-sum-after-deletion.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
     int maxSum(vector<int>& nums) {
-        bool allNegative = true;
-        int maxVal = INT_MIN;
-
-        for (int n : nums) {
-            if (n >= 0) allNegative = false;
-            if (n > maxVal) maxVal = n;
+        unordered_map<int, int> freq;
+        sort(nums.begin(), nums.end(), greater<int> ());
+        if (nums[0] < 0) return nums[0];
+        for (int num : nums) {
+            freq[num]++;
         }
-        if (allNegative) return maxVal;
-
-        unordered_set<int> unique(nums.begin(), nums.end());
-
         int sum = 0;
-        for (int val : unique) {
-            if (val > 0) sum += val;
+        for (const auto &pair : freq) {
+            if (pair.first >= 0) sum += pair.first;
         }
-
         return sum;
+
     }
 };
