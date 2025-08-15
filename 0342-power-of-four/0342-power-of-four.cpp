8fe7754d5ef14d1eab60050459953e2c78466927
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-08-15 13:10:46 +0530

    Time: 0 ms (100%), Space: 7.7 MB (82.36%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.cpp
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
-    bool isPowerOfFour(int n) {
-        if (n<=0) return 0;
-        int exp=31-countl_zero((unsigned)n);
-        return (exp&1)==0 && n==1<<exp;
+    bool isPowerOfFour(unsigned n) {
+        return countl_zero(n)&1 && countl_zero(n)+countr_zero(n)==31;
     }
 };
