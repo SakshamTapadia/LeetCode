41689394d6f2cc4d56dfa7b4e60c0b4e1e11e2a4
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-09-06 20:39:19 +0530

    Time: 0 ms (100%), Space: 183.4 MB (50%) - LeetHub

--- 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
+++ 3744-minimum-operations-to-make-array-elements-zero/3744-minimum-operations-to-make-array-elements-zero.cpp
@@ -1,21 +1,16 @@
-long long expSum4[18]={0};
 class Solution {
 public:
-    static long long expSum(unsigned x){
+    long long expSum4[18]={1};
+    long long expSum(unsigned x){
         if (x==0) return 0;
         int log4=(31-countl_zero(x))/2;
         int r=x-(1<<(2*log4));
         return expSum4[log4]+r*(log4+1LL);
     }
-    static void precompute(){
-        if (expSum4[0]==1) return;
-        expSum4[0]=1;
+    long long minOperations(vector<vector<int>>& queries) {
         for(int i=1; i<18; i++){
             expSum4[i]=expSum4[i-1]+3LL*i*(1LL<<(2*(i-1)))+1;
         }
-    }
-    static long long minOperations(vector<vector<int>>& queries) {
-        precompute();
         long long op=0;
         for(auto& q: queries){
             int l=q[0]-1, r=q[1];
