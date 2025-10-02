584cae48c6cf8e16fbe04dfe30497e94b5d65415
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-10-02 19:06:50 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.88%) - LeetHub

--- 3336-water-bottles-ii/3336-water-bottles-ii.cpp
+++ 3336-water-bottles-ii/3336-water-bottles-ii.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    int maxBottlesDrunk(int numBottles, int numExchange) {
-        int a = 1;
-        int b = 2 * numExchange - 3;
-        int c = -2 * numBottles;
-        double delta = (double)b * b - 4.0 * a * c;
-        int t = (int)ceil((-b + sqrt(delta)) / (2.0 * a));
-        return numBottles + t - 1;
+    int maxBottlesDrunk(int nB, int nE) {
+        int x=nB;
+        int y=nE;
+        int h=(((-2*y)+3+sqrt(4*y*y+8*x-12*y+1))/2);
+        return x+h;
     }
 };
