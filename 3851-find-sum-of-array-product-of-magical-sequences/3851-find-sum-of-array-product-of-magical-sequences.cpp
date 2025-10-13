d27a99aa7b8508a4bf9fd8b971d1ebd184e147b0
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-10-14 02:50:56 +0530

    Time: 102 ms (51.43%), Space: 67.3 MB (44.97%) - LeetHub

--- 3851-find-sum-of-array-product-of-magical-sequences/3851-find-sum-of-array-product-of-magical-sequences.cpp
+++ 3851-find-sum-of-array-product-of-magical-sequences/3851-find-sum-of-array-product-of-magical-sequences.cpp
@@ -1,70 +1,56 @@
 class Solution {
 public:
-    long long quickmul(long long x, long long y, long long mod) {
-        long long res = 1, cur = x % mod;
-        while (y) {
-            if (y & 1) {
-                res = res * cur % mod;
-            }
-            y >>= 1;
-            cur = cur * cur % mod;
-        }
-        return res;
-    };
-
     int magicalSum(int m, int k, vector<int>& nums) {
-        int n = nums.size();
-        const long long mod = 1e9 + 7;
-        vector<long long> fac(m + 1, 1);
-        for (int i = 1; i <= m; i++) {
-            fac[i] = fac[i - 1] * i % mod;
-        }
-        vector<long long> ifac(m + 1, 1);
-        for (int i = 2; i <= m; i++) {
-            ifac[i] = quickmul(i, mod - 2, mod);
-        }
-        for (int i = 2; i <= m; i++) {
-            ifac[i] = ifac[i - 1] * ifac[i] % mod;
-        }
-        vector numsPower(n, vector<long long>(m + 1, 1));
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= m; j++) {
-                numsPower[i][j] = numsPower[i][j - 1] * nums[i] % mod;
-            }
-        }
+        const vector<vector<int>> comb = getComb(m, m);
+        vector<vector<vector<vector<int>>>> mem(
+            m + 1, vector<vector<vector<int>>>(
+                       k + 1, vector<vector<int>>(nums.size() + 1,
+                                                  vector<int>(m + 1, -1))));
+        return dp(m, k, 0, 0, nums, mem, comb);
+    }
 
-        vector f(n,
-                 vector(m + 1, vector(m * 2 + 1, vector<long long>(k + 1, 0))));
-        for (int j = 0; j <= m; j++) {
-            f[0][j][j][0] = numsPower[0][j] * ifac[j] % mod;
+private:
+    static constexpr int kMod = 1'000'000'007;
+    int dp(int m, int k, int i, unsigned carry, const vector<int>& nums,
+           vector<vector<vector<vector<int>>>>& mem,
+           const vector<vector<int>>& comb) {
+        if (m < 0 || k < 0 || (m + popcount(carry) < k))
+            return 0;
+        if (m == 0)
+            return k == popcount(carry) ? 1 : 0;
+        if (i == nums.size())
+            return 0;
+        if (mem[m][k][i][carry] != -1)
+            return mem[m][k][i][carry];
+        int res = 0;
+        for (int count = 0; count <= m; ++count) {
+            const long contribution =
+                comb[m][count] * modPow(nums[i], count) % kMod;
+            const int newCarry = carry + count;
+            res = (res +
+                   static_cast<long>(dp(m - count, k - (newCarry % 2), i + 1,
+                                        newCarry / 2, nums, mem, comb)) *
+                       contribution) %
+                  kMod;
         }
-        for (int i = 0; i + 1 < n; i++) {
-            for (int j = 0; j <= m; j++) {
-                for (int p = 0; p <= m * 2; p++) {
-                    for (int q = 0; q <= k; q++) {
-                        int q2 = p % 2 + q;
-                        if (q2 > k) {
-                            break;
-                        }
-                        for (int r = 0; r + j <= m; r++) {
-                            int p2 = p / 2 + r;
-                            f[i + 1][j + r][p2][q2] += f[i][j][p][q] *
-                                                       numsPower[i + 1][r] %
-                                                       mod * ifac[r] % mod;
-                            f[i + 1][j + r][p2][q2] %= mod;
-                        }
-                    }
-                }
-            }
-        }
-        long long res = 0;
-        for (int p = 0; p <= m * 2; p++) {
-            for (int q = 0; q <= k; q++) {
-                if (__builtin_popcount(p) + q == k) {
-                    res = (res + f[n - 1][m][p][q] * fac[m] % mod) % mod;
-                }
-            }
-        }
-        return res;
+        return mem[m][k][i][carry] = res;
+    }
+
+    vector<vector<int>> getComb(int n, int k) {
+        vector<vector<int>> comb(n + 1, vector<int>(k + 1));
+        for (int i = 0; i <= n; ++i)
+            comb[i][0] = 1;
+        for (int i = 1; i <= n; ++i)
+            for (int j = 1; j <= k; ++j)
+                comb[i][j] = comb[i - 1][j] + comb[i - 1][j - 1];
+        return comb;
+    }
+
+    long modPow(long x, long n) {
+        if (n == 0)
+            return 1;
+        if (n % 2 == 1)
+            return x * modPow(x % kMod, (n - 1)) % kMod;
+        return modPow(x * x % kMod, (n / 2)) % kMod;
     }
 };
