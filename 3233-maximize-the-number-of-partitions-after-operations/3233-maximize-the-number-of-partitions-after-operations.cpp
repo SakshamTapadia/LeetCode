88f5536f33a4f8d607877c0fbc68c98c96be2d2f
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-10-17 23:04:51 +0530

    Time: 519 ms (75.41%), Space: 157.5 MB (32.79%) - LeetHub

--- 3233-maximize-the-number-of-partitions-after-operations/3233-maximize-the-number-of-partitions-after-operations.cpp
+++ 3233-maximize-the-number-of-partitions-after-operations/3233-maximize-the-number-of-partitions-after-operations.cpp
@@ -1,60 +1,42 @@
 class Solution {
 public:
+    unordered_map<long long, int> memo;
+    int solve(int i, string& s, int mask, bool canchange, int k){
+        if(i==s.size()){
+            return 0;
+        }
+        long long key = ((long long)i<<27 | (long long)mask<<1 | (canchange ? 1LL : 0LL));
+
+        if(memo.count(key))
+            return memo[key];
+
+        int bit = 1<<(s[i]-'a');
+        int newmask = bit | mask;
+        int best = 0;
+        if(__builtin_popcount(newmask) > k)
+            best = 1 + solve(i+1, s, bit, canchange, k);
+        else
+            best = solve(i+1, s, newmask, canchange, k);
+        if(canchange){
+            for(int c = 0; c<26;c++){
+                int bit2 = 1<<c;
+                int mask2 = bit2 | mask;
+                int candidates;
+                if(__builtin_popcount(mask2) > k)
+                    candidates = 1 + solve(i+1, s, bit2, false, k);
+                else
+                    candidates = solve(i+1, s, mask2, false, k);
+                best = max(best, candidates);
+            }
+        }
+
+        memo[key] = best;
+        return best;
+    }
+
     int maxPartitionsAfterOperations(string s, int k) {
-        int n = s.length();
-        vector<vector<int>> left(n, vector<int>(3)), right(n, vector<int>(3));
-        int num = 0, mask = 0, count = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int binary = 1 << (s[i] - 'a');
-            if (!(mask & binary)) {
-                count++;
-                if (count <= k) {
-                    mask |= binary;
-                } else {
-                    num++;
-                    mask = binary;
-                    count = 1;
-                }
-            }
-            left[i + 1][0] = num;
-            left[i + 1][1] = mask;
-            left[i + 1][2] = count;
-        }
+        return 1 + solve(0, s, 0, true, k);
+        
 
-        num = 0, mask = 0, count = 0;
-        for (int i = n - 1; i > 0; i--) {
-            int binary = 1 << (s[i] - 'a');
-            if (!(mask & binary)) {
-                count++;
-                if (count <= k) {
-                    mask |= binary;
-                } else {
-                    num++;
-                    mask = binary;
-                    count = 1;
-                }
-            }
-            right[i - 1][0] = num;
-            right[i - 1][1] = mask;
-            right[i - 1][2] = count;
-        }
-
-        int Max = 0;
-        for (int i = 0; i < n; i++) {
-            int seg = left[i][0] + right[i][0] + 2;
-            int totMask = left[i][1] | right[i][1];
-            int totCount = 0;
-            while (totMask) {
-                totMask = totMask & (totMask - 1);
-                totCount++;
-            }
-            if (left[i][2] == k && right[i][2] == k && totCount < 26) {
-                seg++;
-            } else if (min(totCount + 1, 26) <= k) {
-                seg--;
-            }
-            Max = max(Max, seg);
-        }
-        return Max;
     }
 };
