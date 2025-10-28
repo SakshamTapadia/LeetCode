1618c5f07dda13ce9aba55b97912901bebe449ad
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-10-28 15:17:12 +0530

    Time: 5 ms (51.83%), Space: 22 MB (67.89%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
-        int ans = 0;
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int leftSum = 0;
-        int rightSum = sum;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                if (leftSum - rightSum >= 0 && leftSum - rightSum <= 1) {
-                    ans++;
-                }
-                if (rightSum - leftSum >= 0 && rightSum - leftSum <= 1) {
-                    ans++;
-                }
-            } else {
-                leftSum += nums[i];
-                rightSum -= nums[i];
+        int sum = 0, ans = 0, s = 0;
+        for(int i=0 ; i<n ; i++) sum += nums[i];
+        for(int i=0 ; i<n ; i++){
+            s += nums[i];
+            if(nums[i] == 0){
+                if(s == sum-s) ans += 2;
+                else if(abs(sum - 2*s) == 1) ans += 1;
             }
         }
         return ans;
