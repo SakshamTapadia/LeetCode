58b64501f040cc82bc2b497d00e72b0a8a2a46fc
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-10-31 21:01:51 +0530

    Time: 0 ms (100%), Space: 25.3 MB (86.35%) - LeetHub

--- 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.cpp
@@ -1,14 +1,35 @@
 class Solution {
 public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
-        vector<int> res;
-        unordered_map<int, int> count;
-        for (int x : nums) {
-            count[x]++;
-            if (count[x] == 2) {
-                res.push_back(x);
+
+        int xorSum = 0;
+        int totalSize = nums.size();
+        int actualSize = nums.size() - 2;
+        for(int i = 0; i < totalSize; i++) {
+            xorSum ^= nums[i];
+        }
+        for(int i = 0; i < actualSize; i++) {
+            xorSum ^= i;
+        }
+        int rightmostSetBit = xorSum & ~(xorSum - 1);
+
+        int firstSneakyNumber = 0;
+        int secondSneakyNumber = 0;
+        for(int i = 0; i < totalSize; i++) {
+            if(nums[i] & rightmostSetBit) {
+                firstSneakyNumber ^= nums[i];
+            } else {
+                secondSneakyNumber ^= nums[i];
             }
         }
-        return res;
+        for(int i = 0; i < actualSize; i++) {
+            if(i & rightmostSetBit) {
+                firstSneakyNumber ^= i;
+            } else {
+                secondSneakyNumber ^= i;
+            }
+        }
+
+        return {firstSneakyNumber, secondSneakyNumber};
     }
 };
