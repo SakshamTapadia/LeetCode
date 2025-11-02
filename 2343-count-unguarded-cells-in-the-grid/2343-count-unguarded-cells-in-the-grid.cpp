8511e4b131ece7ca6c3d102a3e88729ce824cf83
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-11-02 15:37:04 +0530

    Time: 52 ms (80.28%), Space: 173 MB (80.28%) - LeetHub

--- 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
+++ 2343-count-unguarded-cells-in-the-grid/2343-count-unguarded-cells-in-the-grid.cpp
@@ -1,45 +1,55 @@
 class Solution {
-public:
-    const int UNGUARDED = 0;
-    const int GUARDED = 1;
-    const int GUARD = 2;
-    const int WALL = 3;
-
-    void markguarded(int row, int col, vector<vector<int>>& grid) {
-        for (int r = row - 1; r >= 0; r--) {
-            if (grid[r][col] == WALL || grid[r][col] == GUARD) break;
-            grid[r][col] = GUARDED;
+    void check(vector<vector<int>>& visited, int n, int m, int i, int j)
+    {
+        for(int k=j+1;k<m;k++)
+        {
+            if(visited[i][k] == 1 || visited[i][k] == 2) break;
+            visited[i][k] = 3;
         }
-        for (int r = row + 1; r < grid.size(); r++) {
-            if (grid[r][col] == WALL || grid[r][col] == GUARD) break;
-            grid[r][col] = GUARDED;
+        for(int k=j-1;k>=0;k--)
+        {
+            if(visited[i][k] == 1 || visited[i][k] == 2) break;
+            visited[i][k] = 3;
         }
-        for (int c = col - 1; c >= 0; c--) {
-            if (grid[row][c] == WALL || grid[row][c] == GUARD) break;
-            grid[row][c] = GUARDED;
+        for(int k=i+1;k<n;k++)
+        {
+            if(visited[k][j] == 1 || visited[k][j] == 2) break;
+            visited[k][j] = 3;
         }
-        for (int c = col + 1; c < grid[row].size(); c++) {
-            if (grid[row][c] == WALL || grid[row][c] == GUARD) break;
-            grid[row][c] = GUARDED;
+        for(int k=i-1;k>=0;k--)
+        {
+            if(visited[k][j] == 1 || visited[k][j] == 2) break;
+            visited[k][j] = 3;
         }
     }
+public:
+    int countUnguarded(int n, int m, vector<vector<int>>& guards, vector<vector<int>>& walls) {
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        for(auto& vec : walls)
+        {
+            int x = vec[0];
+            int y = vec[1];
+            visited[x][y] = 1;
+        }
+        for(auto& vec : guards)
+        {
+            int x = vec[0];
+            int y = vec[1];
+            visited[x][y] = 2;
+        }
+        for(auto& vec : guards)
+        {
+            int x = vec[0];
+            int y = vec[1];
+            check(visited, n, m, x, y);
+        }
 
-    int countUnguarded(int m, int n, vector<vector<int>>& guards,
-                       vector<vector<int>>& walls) {
-        vector<vector<int>> grid(m, vector<int>(n, UNGUARDED));
-        for (const auto& guard : guards) {
-            grid[guard[0]][guard[1]] = GUARD;
-        }
-        for (const auto& wall : walls) {
-            grid[wall[0]][wall[1]] = WALL;
-        }
-        for (const auto& guard : guards) {
-            markguarded(guard[0], guard[1], grid);
-        }
         int count = 0;
-        for (const auto& row : grid) {
-            for (const auto& cell : row) {
-                if (cell == UNGUARDED) count++;
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                if(visited[i][j] == 0) count++;
             }
         }
         return count;
