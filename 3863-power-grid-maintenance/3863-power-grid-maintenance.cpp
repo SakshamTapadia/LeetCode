abfd53979b891d402b56ee3b07fce8a76172367e
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-11-06 12:12:04 +0530

    Time: 28 ms (98.59%), Space: 295.1 MB (99.6%) - LeetHub

--- 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
+++ 3863-power-grid-maintenance/3863-power-grid-maintenance.cpp
@@ -1,80 +1,60 @@
-class DSU {
-public:
-    vector<int> parent;
-    DSU(int size) {
-        parent.resize(size);
-        iota(parent.begin(), parent.end(), 0);
-    }
-
-    int find(int x) { return parent[x] == x ? x : parent[x] = find(parent[x]); }
-
-    void join(int u, int v) { parent[find(v)] = find(u); }
-};
-
 class Solution {
 public:
-    vector<int> processQueries(int c, vector<vector<int>>& connections,
-                               vector<vector<int>>& queries) {
-        DSU dsu(c + 1);
+    vector<int> processQueries(int c, vector<vector<int>>& connections, vector<vector<int>>& queries) {
+        vector<int> parent(c + 1);
+        iota(parent.begin(), parent.end(), 0);
 
-        for (auto& p : connections) {
-            dsu.join(p[0], p[1]);
+        function<int(int)> find = [&](int x) {
+            while (parent[x] != x) {
+                parent[x] = parent[parent[x]];
+                x = parent[x];
+            }
+            return x;
+        };
+        for (auto& conn : connections) {
+            int a = conn[0], b = conn[1];
+            int ra = find(a), rb = find(b);
+            if (ra != rb) parent[rb] = ra;
         }
 
-        vector<bool> online(c + 1, true);
-        vector<int> offlineCounts(c + 1, 0);
-        unordered_map<int, int> minimumOnlineStations;
+        vector<int> next_node(c + 1, 0), comp_min(c + 1, 0), last(c + 1, 0);
+        for (int i = 1; i <= c; ++i) {
+            int r = find(i);
+            if (comp_min[r] == 0) {
+                comp_min[r] = i;
+            } else {
+                next_node[last[r]] = i;
+            }
+            last[r] = i;
+        }
+
+        vector<bool> offline(c + 1, false);
+        vector<int> res;
 
         for (auto& q : queries) {
-            int op = q[0], x = q[1];
-            if (op == 2) {
-                online[x] = false;
-                offlineCounts[x]++;
-            }
-        }
+            int t = q[0], x = q[1];
 
-        for (int i = 1; i <= c; i++) {
-            int root = dsu.find(i);
-            if (!minimumOnlineStations.count(root)) {
-                minimumOnlineStations[root] = -1;
-            }
-
-            int station = minimumOnlineStations[root];
-            if (online[i]) {
-                if (station == -1 || station > i) {
-                    minimumOnlineStations[root] = i;
-                }
-            }
-        }
-
-        vector<int> ans;
-
-        for (int i = (int)queries.size() - 1; i >= 0; i--) {
-            int op = queries[i][0], x = queries[i][1];
-            int root = dsu.find(x);
-            int station = minimumOnlineStations[root];
-
-            if (op == 1) {
-                if (online[x]) {
-                    ans.push_back(x);
+            if (t == 1) { 
+                if (!offline[x]) {
+                    res.push_back(x);
                 } else {
-                    ans.push_back(station);
+                    int r = find(x);
+                    res.push_back(comp_min[r] ? comp_min[r] : -1);
                 }
-            }
-
-            if (op == 2) {
-                if (offlineCounts[x] > 1) {
-                    offlineCounts[x]--;
-                } else {
-                    online[x] = true;
-                    if (station == -1 || station > x) {
-                        minimumOnlineStations[root] = x;
+            } 
+            else { 
+                if (!offline[x]) {
+                    offline[x] = true;
+                    int r = find(x);
+                    if (comp_min[r] == x) {
+                        int y = next_node[x];
+                        while (y && offline[y]) y = next_node[y];
+                        comp_min[r] = y ? y : 0;
                     }
                 }
             }
         }
 
-        reverse(ans.begin(), ans.end());
-        return ans;
+        return res;
     }
 };
