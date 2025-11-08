d9cd32f0274636a65680477e91d05bff3a71a6a3
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-11-08 22:09:18 +0530

    Time: 0 ms (100%), Space: 7.9 MB (80.63%) - LeetHub

--- 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.cpp
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/1732-minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        
-        int k = 0;
-        int curr = 1;
-        while (curr * 2 <= n) {
-            curr *= 2;
-            k++;
-        }
-        
-        return (1 << (k + 1)) - 1 - minimumOneBitOperations(n ^ curr);
+        int ans = n;
+        ans ^= ans >> 16;
+        ans ^= ans >> 8;
+        ans ^= ans >> 4;
+        ans ^= ans >> 2;
+        ans ^= ans >> 1;
+        return ans;
     }
 };
