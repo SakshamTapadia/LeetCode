0f3073eb2c77ea5900bb6399faecc224e82f5f12
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-11-09 17:02:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (40.22%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        int res = 0;
-        while (num1 && num2) {
-            res += num1 / num2;
-            num1 %= num2;
-            swap(num1, num2);
+        int cnt = 0, a = max(num1, num2), b = min(num1, num2);
+        while (a && b) {
+            cnt += a / b;
+            a %= b;
+            swap(a, b);
         }
-        return res;
+        return cnt;
     }
 };
