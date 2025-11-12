6f4a641d5dc7331800af5a7e04e13d82ce64a84f
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-11-12 22:44:27 +0530

    Time: 0 ms (100%), Space: 31.8 MB (9.03%) - LeetHub

--- 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
+++ 2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1/2753-minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums) {
-        int n = nums.size();
-        int num1 = 0;
-        int g = 0;
-        for (int x : nums) {
-            if (x == 1) {
-                num1++;
-            }
-            g = gcd(g, x);
-        }
-        if (num1 > 0) {
-            return n - num1;
-        }
-        if (g > 1) {
-            return -1;
-        }
-
-        int min_len = n;
-        for (int i = 0; i < n; i++) {
-            int g = 0;
-            for (int j = i; j < n; j++) {
-                g = gcd(g, nums[j]);
-                if (g == 1) {
-                    min_len = min(min_len, j - i + 1);
+        const int n = nums.size();
+        int res = INT_MAX, cnt1 = 0;
+        for(int i = 0; i < n; ++i) cnt1 += (nums[i] == 1);
+        if(cnt1) return n - cnt1;
+        for(int i = 0; i < n; ++i){
+            int g = nums[i];
+            for(int j = i + 1; j < n; ++j){
+                g = __gcd(g , nums[j]);
+                if(g == 1){
+                    res = min(res , j - i + n - 1);
                     break;
                 }
             }
         }
-        return min_len + n - 2;
+        return res == INT_MAX ? -1 : res;
     }
 };
