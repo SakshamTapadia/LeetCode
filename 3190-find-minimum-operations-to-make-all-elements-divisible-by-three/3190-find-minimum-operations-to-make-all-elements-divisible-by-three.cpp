5644bbecf360064dc4054ec9b01bd57daa9dcd41
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-11-22 17:58:18 +0530

    Time: 0 ms (100%), Space: 23.1 MB (79.62%) - LeetHub

--- 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
+++ 3190-find-minimum-operations-to-make-all-elements-divisible-by-three/3190-find-minimum-operations-to-make-all-elements-divisible-by-three.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int minimumOperations(vector<int>& nums) {
-        return std::accumulate(nums.begin(), nums.end(), 0, [](int acc, int v) {
-            return acc + std::min(v % 3, 3 - v % 3);
+        return accumulate(nums.begin(), nums.end(), 0, [](int acc, int v) {
+            return acc + min(v % 3, 3 - v % 3);
         });
     }
 };
