84409d5f0186e05bafbaba0f5a7b49b8a7d680b1
Saksham Tapadia <114163944+SakshamTapadia@users.noreply.github.com>
2025-12-29 11:24:30 +0530

    Time: 111 ms (87.57%), Space: 16.1 MB (35.68%) - LeetHub

--- 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
+++ 0756-pyramid-transition-matrix/0756-pyramid-transition-matrix.cpp
@@ -1,34 +1,32 @@
 class Solution {
-public:
-    map<string,vector<string>> mp;
-    map<string,bool> dp;
-    bool solve(int ind, string newlayer, string oldlayer, int n)
-    {
-        if(n==1) return true;
-        
-        if(ind==n-1)
-        {
-            if (dp.count(newlayer)) return dp[newlayer];
-            return dp[newlayer]=solve(0,"",newlayer,n-1);
+private:
+std::unordered_map<string, std::vector<char>> allowedTops_;
+std::unordered_map<string, bool> memo_;
+    bool generateNextLevels(string& previous, string current) { 
+        if (previous.size() == 1) { 
+            return true;
         }
-        string tmp = oldlayer.substr(ind,2);
-
-        if(!mp.count(tmp)) return false; 
-        for(auto x:mp[tmp]){
-            if(solve(ind+1,newlayer+x,oldlayer,n))
+        if (previous.length() - 1 == current.length()) {
+            if (memo_.contains(current)) { 
+                return memo_[current];
+            }
+            bool result = generateNextLevels(current, "");
+            memo_[current] = result;
+            return result;
+        }
+        int idx = current.length();
+        for (const auto& top : allowedTops_[previous.substr(idx, 2)]) {
+            if (generateNextLevels(previous, current+top)) { 
                 return true;
+            }
         }
         return false;
     }
+public:
     bool pyramidTransition(string bottom, vector<string>& allowed) {
-        
-        int n = bottom.size();
-
-
-        for(auto x:allowed){
-            mp[x.substr(0,2)].push_back(x.substr(2));
+        for (const auto& triple : allowed) { 
+            allowedTops_[triple.substr(0,2)].push_back(triple.back());
         }
-
-        return solve(0,"",bottom,n);
+        return generateNextLevels(bottom, "");
     }
 };
